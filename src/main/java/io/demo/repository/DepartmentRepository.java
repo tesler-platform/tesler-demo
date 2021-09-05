80073c6082ff4073b328c968270d5206b4bc790d
igornb <igornb.mail@gmail.com>
2021-09-05 10:05:25 +0300

    prepare to tesler 3.0.4

---
+++ src/main/java/io/demo/repository/DepartmentRepository.java
@@ -0,0 +1,11 @@
+package io.demo.repository;
+
+import io.tesler.model.core.entity.Department;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface DepartmentRepository extends JpaRepository<Department, Long>, JpaSpecificationExecutor<Department> {
+
+}
