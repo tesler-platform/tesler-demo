79b911faae7d209556f9db927bcd8220d6378bb3
Daria Mitrofanova <dasha.mitr98@gmail.com>
2022-06-09 19:39:41 +0300

    delete unused annotation

--- src/main/java/io/demo/dto/DashboardFilterDTO.java
+++ src/main/java/io/demo/dto/DashboardFilterDTO.java
@@ -2,8 +2,6 @@ package io.demo.dto;
 
 import io.tesler.api.data.dto.DataResponseDTO;
 import io.tesler.core.dto.multivalue.MultivalueField;
-import io.tesler.core.util.filter.SearchParameter;
-import io.tesler.core.util.filter.provider.impl.StringValueProvider;
 import io.tesler.model.core.entity.User;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -14,7 +12,6 @@ import lombok.Setter;
 @NoArgsConstructor
 public class DashboardFilterDTO extends DataResponseDTO {
 
-	@SearchParameter(name = "fieldOfActivities.value", multiFieldKey = StringValueProvider.class)
 	private MultivalueField fieldOfActivity;
 
 	public DashboardFilterDTO(User user) {
