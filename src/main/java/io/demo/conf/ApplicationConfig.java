f3d01717d9bedc593271231577c98ee7a77c353f
abratashev <abratashev@gmail.com>
2022-10-17 23:43:50 +0300

    Extend uploading and downloading files size to 1GB

--- src/main/java/io/demo/conf/ApplicationConfig.java
+++ src/main/java/io/demo/conf/ApplicationConfig.java
@@ -7,6 +7,7 @@ import io.tesler.core.config.CoreApplicationConfig;
 import io.tesler.core.config.UIConfig;
 import io.tesler.model.core.config.PersistenceJPAConfig;
 import io.tesler.model.core.tx.TeslerJpaTransactionManagerForceActiveAware;
+import java.util.concurrent.Executors;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.ApplicationContext;
@@ -14,7 +15,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.scheduling.concurrent.ConcurrentTaskExecutor;
 import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.web.servlet.config.annotation.AsyncSupportConfigurer;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -41,7 +44,15 @@ public class ApplicationConfig {
 						.allowedOrigins("*")
 						.allowedHeaders("*");
 			}
+			@Override
+			public void configureAsyncSupport(AsyncSupportConfigurer configurer) {
+				configurer.setTaskExecutor(getTaskExecutor());
+			}
 		};
+	}
+	@Bean
+	protected ConcurrentTaskExecutor getTaskExecutor() {
+		return new ConcurrentTaskExecutor(Executors.newFixedThreadPool(5));
 	}
 
 	@Bean
